b9118933ad6e0ff44d151c6733fc7c12a1461259
Himanshu Neema <himanshu@bluedata.com>
2018-03-23 10:37:58 -0700

    experiment to create a 'demo-deployment' with 2 replicas  on Adding a new BlueDataCluster

--- pkg/controller/bluedatacluster/controller.go
+++ pkg/controller/bluedatacluster/controller.go
@@ -4,6 +4,11 @@
 package bluedatacluster
 
 import (
+    "k8s.io/client-go/kubernetes"
+    "k8s.io/client-go/rest"
+    appsv1beta1 "k8s.io/api/apps/v1beta1"
+    apiv1 "k8s.io/api/core/v1"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
     "log"
     "github.com/kubernetes-sigs/kubebuilder/pkg/builders"
     "bluedata-apiserver-extension/pkg/apis/bluedata/v1alpha1"
@@ -30,6 +35,8 @@ type BlueDataClusterControllerImpl struct {
     lister listers.BlueDataClusterLister
 }
 
+func int32Ptr(i int32) *int32 { return &i }
+
 // Init initializes the controller and is called by the generated code
 // Register watches for additional resource types here.
 func (c *BlueDataClusterControllerImpl) Init(arguments sharedinformers.ControllerInitArguments) {
@@ -48,13 +55,70 @@ func (c *BlueDataClusterControllerImpl) Init(arguments sharedinformers.Controlle
     //     arguments.GetSharedInformers().Factory.Bar().V1beta1().Bars().Informer(),
     //     c.FooToBlueDataCluster)
 
-    erc := EpicRestClient{}
+
+    //erc := EpicRestClient{}
     arguments.GetSharedInformers().Factory.Bluedata().V1alpha1().BlueDataClusters().Informer().
   		AddEventHandler(cache.ResourceEventHandlerFuncs{
              AddFunc: func(obj interface{}) {
                     bluedatacluster, _ := obj.(*v1alpha1.BlueDataCluster)
                     log.Printf("ADD BlueDataCluster: %s", bluedatacluster.Name)
-                    erc.CreateCluster(bluedatacluster)
+                    //erc.CreateCluster(bluedatacluster)
+                    // creates the in-cluster config
+                    config, err := rest.InClusterConfig()
+                    if err != nil {
+                      log.Printf("ERR: ", err)
+                      return
+                      //panic(err.Error())
+                    }
+
+                    // creates the clientset
+                    clientset, err := kubernetes.NewForConfig(config)
+                    if err != nil {
+                      log.Printf(err.Error())
+                    }
+                    namespace := bluedatacluster.ObjectMeta.Namespace
+                    deploymentsClient := clientset.AppsV1beta1().Deployments(namespace)
+                  	deployment := &appsv1beta1.Deployment{
+                  		ObjectMeta: metav1.ObjectMeta{
+                  			Name: "demo-deployment",
+                        Namespace: namespace,
+                  		},
+                  		Spec: appsv1beta1.DeploymentSpec{
+                  			Replicas: int32Ptr(2),
+                  			Template: apiv1.PodTemplateSpec{
+                  				ObjectMeta: metav1.ObjectMeta{
+                  					Labels: map[string]string{
+                  						"app": "demo",
+                  					},
+                            Namespace: namespace,
+                  				},
+                  				Spec: apiv1.PodSpec{
+                  					Containers: []apiv1.Container{
+                  						{
+                  							Name:  "web",
+                  							Image: "nginx:1.12",
+                  							Ports: []apiv1.ContainerPort{
+                  								{
+                  									Name:          "http",
+                  									Protocol:      apiv1.ProtocolTCP,
+                  									ContainerPort: 80,
+                  								},
+                  							},
+                  						},
+                  					},
+                  				},
+                  			},
+                  		},
+                  	}
+
+                  	// Create Deployment
+                  	log.Println("Creating deployment...")
+                  	result, err := deploymentsClient.Create(deployment)
+                  	if err != nil {
+                  		log.Printf(err.Error())
+                  	}
+                  	log.Printf("Created deployment %q.\n", result.GetObjectMeta().GetName())
+
              },
              DeleteFunc: func(obj interface{}) {
                     bluedatacluster, _ := obj.(*v1alpha1.BlueDataCluster)
