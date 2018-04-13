4b35a4068d12556ab03497bec1fd5686f501254e
Himanshu Neema <himanshu@bluedata.com>
2018-04-13 10:13:58 -0700

    Epic for a namespace

---
+++ epic/setup_namespace.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Required parameters
+# CPU
+# Memory
+# Storage
+# Namespace
+
+CPU="1"
+MEMORY="1Gi"
+STORAGE="40Gi"
+NAMESPACE="default"
+TEMPLATE=epic-3.4-176-namespace-template.yaml
+
+while getopts c:m:s:n: option; do 
+    case $option in 
+       c) 
+          CPU=$OPTARG
+          ;;
+       m) 
+          MEMORY=$OPTARG
+          ;;
+       s) 
+          STORAGE=$OPTARG
+          ;;
+       n) 
+          NAMESPACE=$OPTARG
+          ;;
+       \?)
+          echo "Invalid Option"
+          exit 1
+    esac
+done
+
+echo "NAMESPACE: $NAMESPACE, CPU: $CPU, MEMORY: $MEMORY, STORAGE: $STORAGE"
+
+FILE=epic-for-$NAMESPACE.yaml
+cp $TEMPLATE $FILE
+sed -i "s/{{{{CPU}}}}/$CPU/g ; s/{{{{MEMORY}}}}/$MEMORY/g; s/{{{{STORAGE}}}}/$STORAGE/g; s/{{{{NAMESPACE}}}}/$NAMESPACE/g" $FILE
+echo "Generated $FILE"
